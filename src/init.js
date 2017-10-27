9a87f67e52b67a0bceb24bc0ee97fc5a34e4a4a3
StringEisen <eisennoah@outlook.com>
2017-10-27 16:33:17 +0800

    be nice

--- src/init.js
+++ src/init.js
@@ -42,7 +42,18 @@ function remove_injected() {
     var injected = d3.selectAll("script");
     console.log(injected.length);//undefined
     if (injected.length > 1) {
-        var removed = injected.remove(1); 
+        var removed = injected.remove(1);
         console.log("removed: " + removed.attr("src"));
+    }
+}
+
+// remove js or css file
+function removejscssfile(filename, filetype) {
+    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none" //determine element type to create nodelist from
+    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects = document.getElementsByTagName(targetelement)
+    for (var i = allsuspects.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
+        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
+            allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
     }
 }
